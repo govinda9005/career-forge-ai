b4551b2cbb4e6648d4d7294743daee1ab103f3ba
Govinda Yadav <govindayadav9005@gmail.com>
2026-06-16 16:45:52 +0545

    Blacklist Model Schema & Logout API Implementation

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-
+const tokenBlacklistModel = require('../models/blacklist.model');
 
 function authUser(req, res, next) {
     try {
@@ -8,13 +8,25 @@ function authUser(req, res, next) {
             return res.status(401).json({ message: 'No token provided' });
         }
 
-        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        // Check if the token is blacklisted
+        tokenBlacklistModel.findOne({ token }, (err, blacklistedToken) => {
             if (err) {
-                return res.status(401).json({ message: 'Invalid token' });
-            }            
+                console.error('Error checking token blacklist:', err);
+                return res.status(500).json({ message: 'Internal server error' });
+            }
 
-            req.user = decoded;
-            next();
+            if (blacklistedToken) {
+                return res.status(401).json({ message: 'Token has been blacklisted' });
+            }
+
+            jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).json({ message: 'Invalid token' });
+                }            
+
+                req.user = decoded;
+                next();
+            });
         });
     } catch (error) {
         console.error(error);
